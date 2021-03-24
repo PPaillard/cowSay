d9ab0ca8086d0df6bd13775f2407023b386bb36c
Pierre Paillard <paillard.pierre@gmail.com>
2021-03-24 16:20:02 +0100

    ajout commentaire

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const mesInformations = require ('./information.js');
-
+const mesInformations = require("./information.js");
 
 const cowsay = require("cowsay");
 
-console.log(cowsay.say({
-    text : mesInformations(),
-    e : "oO",
-    T : "U "
-}));
+console.log(
+  cowsay.say({
+    text: mesInformations(),
+    e: "oO",
+    T: "U ",
+  })
+);
+
+// blabla
